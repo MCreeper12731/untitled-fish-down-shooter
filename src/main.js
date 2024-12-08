c5b13b829301f9bbf0a296bbd0dc6288b871d98c
MajBregar <maj.bregar@gmail.com>
2024-12-08 22:02:59 +0100

    removed temporary files

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { ResizeSystem } from 'engine/systems/ResizeSystem.js';
 import { UpdateSystem } from 'engine/systems/UpdateSystem.js';
 import { Game } from './game_engine/Game.js';
-import { GameInstance_tool } from './game_engine/GameInstance.js';
 
 const game = new Game();
 await game.load();
-
-console.log(game.instances);
 
 //necessary to have these in this specific script
 const canvas = game.get_canvas();
